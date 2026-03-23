12507cf6f1217c498ddb1c7ff1f45b51929cdee7
Isacintra <isabellecintra43@gmail.com>
2026-03-23 14:50:33 -0300

    variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,18 @@
+#Atividade1
+nome = "Isabelle"
+
+print ("Seu nome é:", nome)
+
+#Atividade2
+idade = 18
+ano = 2026
+
+print ("Sua idade é:", idade)
+print ("O ano atual é:", ano)
+
+#Atividade3
+a = 7
+b = 12
+soma = a + b
+
+print ("A soma de", a, "e", b, "é:", soma)
